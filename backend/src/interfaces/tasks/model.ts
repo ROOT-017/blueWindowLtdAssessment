14a2519bc1da984605b071f9c4a7a403c8a1dc79
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2025-06-12 19:58:55 +0100

    frontend code

--- backend/src/interfaces/tasks/model.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface ITaskModel extends Document {
-  id: number;
-  title: string;
-  description: string;
-  created_at: Date;
-  updated_at: Date;
-}
