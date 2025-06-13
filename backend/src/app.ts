e5681a82f8c75a0d03640b0383bf95ec0a257a6c
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2025-06-13 23:30:58 +0100

    morgan remove on prod

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,14 @@
-import express, { Application} from "express";
+import express, { Application } from "express";
 import morgan from "morgan";
 import { requestLogger } from "./utils/logger";
 import taskRouter from "./routes/tasks";
 
 const app: Application = express();
 
-
 app.use(requestLogger);
-app.use(morgan("dev"));
+process.env.NODE_ENV !== "production" && app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/tasks", taskRouter);
-
-
 
 export default app;
