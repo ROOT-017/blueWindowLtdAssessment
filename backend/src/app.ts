f9ea8ec4f7e4b3865afe155050ab5f22ca234dda
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2025-06-13 23:44:22 +0100

    Core added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,24 @@ import express, { Application } from "express";
 import morgan from "morgan";
 import { requestLogger } from "./utils/logger";
 import taskRouter from "./routes/tasks";
+import cors from "cors";
+import helmet from "helmet";
 
 const app: Application = express();
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(helmet());
+
+app.use(cors(corsOptions));
+
+// Handle preflight requests
+app.options("*", cors(corsOptions));
 
 app.use(requestLogger);
 process.env.NODE_ENV !== "production" && app.use(morgan("dev"));
